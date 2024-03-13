4d649b4475c2b63e23dcebb11dfc1947ea91329b
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-03-13 13:47:37 +0600

    blogs data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useEffect } from 'react';
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Header from './components/Header/Header'
+import { useState } from 'react';
 
 function App() {
-
+ 
 
   return (
     <>
      
       <Header></Header>
-      
+      <Blogs></Blogs>
     
     </>
   )
